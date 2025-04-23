ea8a46b48eec2dd618a5a97b616c8114dd717c63
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-04-23 11:27:03 -0600

    Add: Test to main to test as I code HashMap class

---
+++ main.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# main contains the main execution
+# for running my HashMaps project.
+#
+
+test = HashMap.new(0.75, 16)
+
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
