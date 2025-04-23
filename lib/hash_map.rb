fe0c3cbe54d3684160b2e4f1340149b3a9410a73
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-04-23 11:29:11 -0600

    Add: Odin Project-given hash method

--- lib/hash_map.rb
+++ lib/hash_map.rb
@@ -13,4 +13,13 @@ class HashMap
     @capacity = capacity
   end
 
+  def hash(key)
+    hash_code = 0
+    prime_number = 31
+       
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+       
+    hash_code
+  end
+ 
 end
